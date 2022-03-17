5166fabb73622e6f4e2541209f4fb133d537c40c
Smartestguy88 <verysmartguy88@gmail.com>
2022-03-17 15:14:24 +1000

    First Commit

---
+++ Actual Scripts/EasyDiscord.py
@@ -0,0 +1,7 @@
+# All scripts by Smartguy88 (Discord Smartguy88#4909)
+# gg gl :)
+
+## If you are looking for a template, you are looking in the right place!
+## Note this program can only run one bot at a time in the current version
+
+# Looking for the github? Here it is: 
